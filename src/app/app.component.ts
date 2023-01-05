39b120e1e457bb8790aa8bfb62930208910295ca
BlenyBonze <blenybonze@hotmail.com>
2023-01-05 08:52:53 -0300

    Instalando PrimeNg para estilização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+
+  nome: string = "fernando";
+
+  mostrarNome() {
+    console.log(this.nome)
+  }
 }
